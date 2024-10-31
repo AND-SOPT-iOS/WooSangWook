47c621dda615ba7e4deffded877b107512303ca0
sangwook <operawook@gmail.com>
2024-10-31 13:37:34 +0900

    [feat] week2

---
+++ iOS35/Presentation/Main/Model/AppStoreChip.swift
@@ -0,0 +1,20 @@
+//
+//  AppStoreChip.swift
+//  iOS35
+//
+//  Created by 우상욱 on 10/26/24.
+//
+
+import Foundation
+
+struct AppStoreChip{
+    let top: String
+    let middle: String
+    let bottom: String
+    
+    init(top: String, middle: String, bottom: String) {
+        self.top = top
+        self.middle = middle
+        self.bottom = bottom
+    }
+}
---
+++ iOS35/Presentation/Main/Model/UpdateState.swift
@@ -0,0 +1,13 @@
+//
+//  UpdateState.swift
+//  iOS35
+//
+//  Created by 우상욱 on 10/25/24.
+//
+
+import UIKit
+
+enum UpdateState {
+    case latest
+    case required
+}
