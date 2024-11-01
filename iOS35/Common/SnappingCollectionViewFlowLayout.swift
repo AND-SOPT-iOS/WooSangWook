d6b6452ff1bf6fd8029e9a72ed80d25a874d9610
sangwook <operawook@gmail.com>
2024-11-01 23:31:26 +0900

    [feat] #5 snapping collection view flow layout

---
+++ iOS35/Common/SnappingCollectionViewFlowLayout.swift
@@ -0,0 +1,30 @@
+//
+//  SnappingCollectionViewFlowLayout.swift
+//  iOS35
+//
+//  Created by 우상욱 on 10/31/24.
+//
+
+import UIKit
+
+class SnappingCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+           guard let collectionView = collectionView else { return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity) }
+
+           var offsetAdjustment = CGFloat.greatestFiniteMagnitude
+           let horizontalOffset = proposedContentOffset.x + collectionView.contentInset.left
+
+           let targetRect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView.bounds.size.width, height: collectionView.bounds.size.height)
+
+           let layoutAttributesArray = super.layoutAttributesForElements(in: targetRect)
+
+           layoutAttributesArray?.forEach({ (layoutAttributes) in
+               let itemOffset = layoutAttributes.frame.origin.x
+               if fabsf(Float(itemOffset - horizontalOffset)) < fabsf(Float(offsetAdjustment)) {
+                   offsetAdjustment = itemOffset - horizontalOffset
+               }
+           })
+
+           return CGPoint(x: proposedContentOffset.x + offsetAdjustment, y: proposedContentOffset.y)
+       }
+}
